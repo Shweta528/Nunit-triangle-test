6f6e7f2b09720592c5824bf1f1345b9b2b54aa42
shwet <shwet@SHWETA>
2024-06-12 16:39:04 -0400

    Add project files.

---
+++ Nunit triangle test/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int angle1, int angle2, int angle3)
+        {
+            string result;
+            if ((angle1 + angle2 + angle3) == 180)   
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
---
+++ Shwetatestcode/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitDemonstration;
+
+namespace shwetatestcase
+{
+    [TestFixture]
+    public class Class1
+    {
+        // Shweta Triangle Test Code
+        [Test]
+        public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+            //Arrange
+            int angle1 = 60;
+            int angle2 = 60;
+            int angle3 = 60;
+
+            string expected = "The triangle is valid.";
+
+            //Act
+            string actual = Triangle.ValidTriangle(angle1, angle2, angle3);
+
+            //Assert
+            //Assert.Equals(expected, actual);
+        }
+    }
+} 
